4921a303d5b707452de1e900fa820cf4bb16e5ed
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-01-17 14:36:34 +0530

    updated buildsh

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+# 🔥 Create superuser automatically (if not exists)
+python manage.py createsuperuser \
+  --noinput || true
